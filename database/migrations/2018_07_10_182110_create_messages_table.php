4a2d2499e45b798145db9007f78e4b56e0717aa7
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-10 15:45:09 -0400

    database

---
+++ database/migrations/2018_07_10_182110_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            //primary key
+            $table->increments('id');
+            //Fecha de creacion y ultima modificacion
+            $table->timestamps();
+
+            $table->string('content');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
