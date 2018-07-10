11dc4d64abb6e1650ed8db4b432da213f7b7f44b
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-10 16:53:38 -0400

    form para crear mensaje

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,13 @@
         </nav>
     </div>
     <div class="row">
+        <form action="message/create" method="POST">
+            <div class="form-group">
+                <input type="text" name="message" class="form-control" placeholder="Qué estás pensando?">
+            </div>
+        </form>
+    </div>
+    <div class="row">
         @forelse ($messages as $message)
             <div class="col-6">
             <img class="img-thumbnail" src="{{$message->image}}">
