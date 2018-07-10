1ddb4486a0ba0a811065daf951c54db1e5ca408d
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-10 02:08:36 -0400

    Laratter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title','Zaidcodes')</title>
+        <title>@yield('title','Laratter')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -25,7 +25,7 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div class="container">
                 @yield('content')
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="title m-b-md">
-        Julio Diaz - <a href="about" target="_blank">Zaidcodes</a>
+    <div class="jumbotron text-center">
+        <h1>Laratter</h1>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">About</a>
+                </li>
+            </ul>
+        </nav>
     </div>
-    @if (isset($teacher))
-        <p>Profesor: {{ $teacher }}</p>
-    @else
-        <p>Profesor: Por definir</p>
-    @endif
-    <div class="links">
-        @foreach ($links as $link => $text)
-            <a href="{{ $link }}"> {{$text}} </a>
-        @endforeach
-    </div>    
 @endsection
 
 @section('title')
-    Welcome | Zaidcodes
+    Home | Laratter
 @endsection
