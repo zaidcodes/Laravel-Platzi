d99de6281f5c237fbf7799ad6cf66c72d3a41675
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-11 17:58:18 -0400

    conversacion entre dos usuarios

--- app/Conversation.php
+++ app/Conversation.php
@@ -13,6 +13,21 @@ class Conversation extends Model
     }
 
     public function privateMessages(){
-        return $this->hasMany(PrivateMessage::class);
+        return $this->hasMany(PrivateMessage::class)->orderBy('created_at', 'desc');
+    }
+
+    public static function between(User $user, User $other){
+        $query =  Conversation::whereHas('users', function($query) use ($user){
+            $query->where('user_id',$user->id);
+        })->whereHas('users', function($query) use($other){
+            $query->where('user_id', $other->id);
+        });
+
+        $conversation = $query->firstOrCreate([]);
+        //Garatiza que los usuarios estan en la conversacion y si no estan, los agrega
+        $conversation->users()->sync([
+            $user->id, $other->id
+        ]);
+        return $conversation;
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -55,10 +55,8 @@ class UsersController extends Controller
 
         $me = $request->user();
         $message = $request->input('message');
-        
-        $conversation = Conversation::create();
-        $conversation->users()->attach($me);
-        $conversation->users()->attach($user);
+        //Hay ya una conversacion?
+        $conversation = Conversation::between($me,$user);
 
         $privateMessage = PrivateMessage::create([
             'conversation_id' => $conversation->id,
