38720e058dc8b113912ce6ad3c74ee4ae951dacd
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-11 01:02:25 -0400

    Mostrando fecha de creacion

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,6 +1,11 @@
 <img class="img-thumbnail" src="{{$message->image}}">
 <p class="card-text">
-<div class="text-muted">Escrito por <a href="/{{ $message->user->username }}">{{$message->user->name}}</a></div>
+    <div class="text-muted">Escrito por 
+        <a href="/{{ $message->user->username }}">
+            {{$message->user->name}}
+        </a>
+        <span class="card-text text-muted">en {{ $message->created_at }}</span>
+    </div>
     {{ $message->content }}
     <a href="/messages/{{ $message->id }}">More...</a>
 </p>
