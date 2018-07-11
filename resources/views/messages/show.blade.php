4c2895cb2683f78657b6b232008455d1f929fe25
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-11 00:34:51 -0400

    Extension de mensages

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
     <h1 class="h3">Mensaje id: {{ $message->id }}</h1>
-    <img class="img-thumbnail" src="{{ $message->image }}">
-    <p class="card-text">
-        {{ $message->content }}
-    </p>
+    @include('messages.message')
 <small class="text-muted">Created at: {{ $message->created_at }}</small>
 @endsection
